1d4b1e692cde38ec6fcdc54bb1f902b9753d33ab
sjcl <fruit@bebehp.com>
2017-06-26 03:36:28 +0900

    config

--- eewbot/src/main/java/net/teamfruit/eewbot/Config.java
+++ eewbot/src/main/java/net/teamfruit/eewbot/Config.java
@@ -1,6 +1,22 @@
 package net.teamfruit.eewbot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Config {
 
 	public String token;
+	public int kyoshinDelay;
+	public int timeFixDelay;
+	public List<Channel> channels;
+
+	public static Config getDefault() {
+		final Config c = new Config();
+		c.token = "";
+		c.kyoshinDelay = 1;
+		c.timeFixDelay = 86400;
+		c.channels = new ArrayList<>();
+		return c;
+	}
+
 }
