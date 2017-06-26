b93bc9d44204104d8df5d092883003b96b4153d1
sjcl <fruit@bebehp.com>
2017-06-26 12:11:54 +0900

    channel id

--- eewbot/src/main/java/net/teamfruit/eewbot/Channel.java
+++ eewbot/src/main/java/net/teamfruit/eewbot/Channel.java
@@ -1,7 +1,16 @@
 package net.teamfruit.eewbot;
 
 public class Channel {
-	public long id;
+	private final long id;
 	public boolean eewAlart;
 	public boolean eewPrediction;
+
+	public Channel(final long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
 }
