d21160ff3169f0060b2d939b7e3445e239c6e6ee
sjcl <fruit@bebehp.com>
2017-07-06 23:02:33 +0900

    everyone permission

--- eewbot/src/main/java/net/teamfruit/eewbot/Permission.java
+++ eewbot/src/main/java/net/teamfruit/eewbot/Permission.java
@@ -1,16 +1,25 @@
 package net.teamfruit.eewbot;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Permission {
 
-	public static final Permission EVERYONE = new Permission();
+	public static final Permission EVERYONE = new Permission("everyone", null, Arrays.asList("details", "monitor"));
 
-	private final String group = "everyone";
+	private String group;
 	private List<Long> userid;
 	private List<String> command;
+
+	public Permission() {
+	}
+
+	public Permission(final String group, final List<Long> userid, final List<String> command) {
+		this.group = group;
+		this.userid = userid;
+		this.command = command;
+	}
 
 	public String getGroup() {
 		return this.group;
@@ -19,13 +28,12 @@ public class Permission {
 	public List<Long> getUserid() {
 		if (this.userid!=null)
 			return this.userid;
-		return this.userid = new ArrayList<>();
+		return this.userid = Collections.emptyList();
 	}
 
 	public List<String> getCommand() {
 		if (this.command!=null)
 			return this.command;
-		return this.command = Arrays.asList("details", "monitor");
+		return this.command = Collections.emptyList();
 	}
-
 }
