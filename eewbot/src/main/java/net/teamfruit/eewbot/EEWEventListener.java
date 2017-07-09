057aced9001f20cec5fe9596e3a970e8027390bc
sjcl <fruit@bebehp.com>
2017-07-09 15:19:40 +0900

    subscribe quakeinfoevent

--- eewbot/src/main/java/net/teamfruit/eewbot/EEWEventListener.java
+++ eewbot/src/main/java/net/teamfruit/eewbot/EEWEventListener.java
@@ -2,11 +2,14 @@ package net.teamfruit.eewbot;
 
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import net.teamfruit.eewbot.dispatcher.EEWDispatcher.EEW;
 import net.teamfruit.eewbot.dispatcher.EEWEvent;
 import net.teamfruit.eewbot.dispatcher.MonitorEvent;
+import net.teamfruit.eewbot.dispatcher.QuakeInfoDispather.QuakeInfo;
+import net.teamfruit.eewbot.dispatcher.QuakeInfoEvent;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.api.internal.json.objects.EmbedObject;
 import sx.blah.discord.handle.obj.IChannel;
@@ -32,6 +35,22 @@ public class EEWEventListener {
 	}
 
 	@EventSubscriber
+	public void onQuakeInfo(final QuakeInfoEvent e) {
+		final QuakeInfo info = e.getQuakeInfo();
+		for (final Iterator<Entry<Long, CopyOnWriteArrayList<Channel>>> it1 = EEWBot.instance.getChannels().entrySet().iterator(); it1.hasNext();) {
+			final Entry<Long, CopyOnWriteArrayList<Channel>> entry = it1.next();
+			for (final Iterator<Channel> it2 = entry.getValue().iterator(); it2.hasNext();) {
+				final Channel channel = it2.next();
+				if (channel.quakeInfo) {
+					final IGuild id = EEWBot.instance.getClient().getGuildByID(entry.getKey());
+					final IChannel c = id.getChannelByID(channel.getId());
+					c.sendMessage(buildEmbed(info));
+				}
+			}
+		}
+	}
+
+	@EventSubscriber
 	public void onMonitor(final MonitorEvent e) {
 		for (final Iterator<Entry<Long, CopyOnWriteArrayList<Channel>>> it1 = EEWBot.instance.getChannels().entrySet().iterator(); it1.hasNext();) {
 			final Entry<Long, CopyOnWriteArrayList<Channel>> entry = it1.next();
@@ -40,10 +59,8 @@ public class EEWEventListener {
 				final IGuild id = EEWBot.instance.getClient().getGuildByID(entry.getKey());
 				final IChannel c = id.getChannelByID(channel.getId());
 				c.sendFile("", e.getImage(), "kyoshinmonitor.png");
-
 			}
 		}
-
 	}
 
 	public static EmbedObject buildEmbed(final EEW eew) {
@@ -62,6 +79,23 @@ public class EEWEventListener {
 		builder.withTimestamp(eew.getReportTime().getTime());
 
 		builder.withFooterText("新強震モニタ");
+		return builder.build();
+	}
+
+	public static EmbedObject buildEmbed(final QuakeInfo info) {
+		final EmbedBuilder builder = new EmbedBuilder();
+
+		builder.appendField("震央", info.getEpicenter(), true);
+		builder.appendField("深さ", info.getDepth(), true);
+		builder.appendField("マグニチュード", String.valueOf(info.getMagnitude()), true);
+		builder.appendField("最大震度", info.getMaxIntensity().toString(), false);
+
+		builder.withColor(128, 128, 128);
+		builder.withTitle("地震情報");
+		builder.withTimestamp(info.getAnnounceTime().getTime());
+		Optional.ofNullable(info.getImageUrl()).ifPresent(url -> builder.withImage(url));
+
+		builder.withFooterText("地震情報 - Yahoo!天気・災害");
 		return builder.build();
 	}
 }
