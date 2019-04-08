2c2eda539fd176434d41558c97537850bf1d7bf9
sjcl <fruit@bebehp.com>
2019-04-08 21:25:38 +0900

    Discord4J v3 client

--- src/main/java/net/teamfruit/eewbot/EEWBot.java
+++ src/main/java/net/teamfruit/eewbot/EEWBot.java
@@ -8,9 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHeaders;
@@ -24,31 +21,25 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import net.teamfruit.eewbot.dispatcher.EEWDispatcher;
-import net.teamfruit.eewbot.dispatcher.NTPDispatcher;
-import net.teamfruit.eewbot.dispatcher.QuakeInfoDispather;
+import discord4j.core.DiscordClient;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.event.domain.guild.GuildCreateEvent;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import net.teamfruit.eewbot.registry.Channel;
 import net.teamfruit.eewbot.registry.Config;
 import net.teamfruit.eewbot.registry.ConfigurationRegistry;
 import net.teamfruit.eewbot.registry.Permission;
-import sx.blah.discord.Discord4J.Discord4JLogger;
-import sx.blah.discord.api.ClientBuilder;
-import sx.blah.discord.api.IDiscordClient;
-import sx.blah.discord.api.events.EventSubscriber;
-import sx.blah.discord.handle.impl.events.ReadyEvent;
 
 public class EEWBot {
 	public static EEWBot instance;
 
 	public static final Gson GSON = new GsonBuilder()
 			.registerTypeAdapter(Channel.class, new Channel.ChannelTypeAdapter())
-			//			.setPrettyPrinting()
 			.create();
 
 	public static final String DATA_DIRECTORY = System.getenv("DATA_DIRECTORY");
 	public static final String CONDIG_DIRECTORY = System.getenv("CONFIG_DIRECTORY");
 
-	private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(2, r -> new Thread(r, "EEWBot-communication-thread"));
 	private final ConfigurationRegistry<Config> config = new ConfigurationRegistry<>(CONDIG_DIRECTORY!=null ? Paths.get(CONDIG_DIRECTORY, "config.json") : Paths.get("config.json"), () -> new Config(), Config.class);
 	private final ConfigurationRegistry<Map<Long, List<Channel>>> channels = new ConfigurationRegistry<>(DATA_DIRECTORY!=null ? Paths.get(DATA_DIRECTORY, "channels.json") : Paths.get("channels.json"), () -> new ConcurrentHashMap<Long, List<Channel>>(), new TypeToken<Map<Long, Collection<Channel>>>() {
 	}.getType());
@@ -69,15 +60,15 @@ public class EEWBot {
 			.setDefaultHeaders(Arrays.asList(new BasicHeader(HttpHeaders.ACCEPT_CHARSET, "UTF-8")))
 			.setConnectionManager(this.manager)
 			.build();
-	private IDiscordClient client;
+
+	private DiscordClient client;
+	private EEWService service;
+	private EEWExecutor executor;
 
 	public void initialize() throws IOException {
 		this.config.init();
 		this.channels.init();
 		this.permissions.init();
-
-		if (getConfig().isDebug())
-			((Discord4JLogger) Log.logger).setLevel(Discord4JLogger.Level.DEBUG);
 
 		final String token = System.getenv("TOKEN");
 		if (token!=null)
@@ -88,25 +79,27 @@ public class EEWBot {
 			return;
 		}
 
-		this.client = new ClientBuilder()
-				.withToken(getConfig().getToken())
-				.registerListeners(this, new DiscordEventListener(), new EEWEventListener())
-				.login();
-	}
+		this.client = new DiscordClientBuilder(getConfig().getToken()).build();
 
-	@EventSubscriber
-	public void onReady(final ReadyEvent event) {
-		this.executor.scheduleAtFixedRate(NTPDispatcher.INSTANCE, 0, getConfig().getTimeFixDelay()>=3600 ? getConfig().getTimeFixDelay() : 3600, TimeUnit.SECONDS);
-		this.executor.scheduleAtFixedRate(EEWDispatcher.INSTANCE, 0, getConfig().getKyoshinDelay()>=1 ? getConfig().getKyoshinDelay() : 1, TimeUnit.SECONDS);
-		this.executor.scheduleAtFixedRate(QuakeInfoDispather.INSTANCE, 0, getConfig().getQuakeInfoDelay()>=10 ? getConfig().getQuakeInfoDelay() : 10, TimeUnit.SECONDS);
-		Log.logger.info("Connected!");
-		Log.logger.info("Server List:");
-		event.getClient().getGuilds().forEach(guild -> Log.logger.info(" - "+guild.getName()));
+		this.client.getEventDispatcher().on(ReadyEvent.class)
+				.subscribe(event -> Log.logger.info("Connecting %s guilds...", event.getGuilds().size()));
 
-	}
+		this.service = new EEWService(this.client, getChannels());
+		this.executor = new EEWExecutor(this.service, getConfig());
 
-	public ScheduledExecutorService getExecutor() {
-		return this.executor;
+		this.client.getEventDispatcher().on(ReadyEvent.class)
+				.map(event -> event.getGuilds().size())
+				.flatMap(size -> this.client.getEventDispatcher()
+						.on(GuildCreateEvent.class)
+						.take(size)
+						.collectList())
+				.subscribe(events -> {
+					this.executor.init();
+
+					Log.logger.info("Connected!");
+				});
+
+		this.client.login().block();
 	}
 
 	public Config getConfig() {
@@ -137,8 +130,16 @@ public class EEWBot {
 		return this.http;
 	}
 
-	public IDiscordClient getClient() {
+	public DiscordClient getClient() {
 		return this.client;
+	}
+
+	public EEWService getService() {
+		return this.service;
+	}
+
+	public EEWExecutor getExecutor() {
+		return this.executor;
 	}
 
 	public static void main(final String[] args) throws Exception {
