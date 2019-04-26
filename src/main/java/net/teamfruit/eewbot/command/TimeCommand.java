fd4f1032f07a5609807744f8b2139c3b3dfc156a
sjcl <fruit@bebehp.com>
2019-04-26 14:52:15 +0900

    add time command

---
+++ src/main/java/net/teamfruit/eewbot/command/TimeCommand.java
@@ -0,0 +1,30 @@
+package net.teamfruit.eewbot.command;
+
+import java.awt.Color;
+import java.time.ZonedDateTime;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import net.teamfruit.eewbot.EEWBot;
+import net.teamfruit.eewbot.TimeProvider;
+import reactor.core.publisher.Mono;
+
+public class TimeCommand implements ICommand {
+
+	@Override
+	public Mono<Void> execute(final EEWBot bot, final MessageCreateEvent event, final String[] args) {
+		return event.getMessage().getChannel()
+				.flatMap(channel -> channel.createEmbed(embed -> embed.setTitle("時刻同期")
+						.setColor(new Color(7506394))
+						.addField("最終同期(コンピューター)", bot.getExecutor().getProvider().getLastComputerTime()
+								.map(ZonedDateTime::toString)
+								.orElse("未同期"), false)
+						.addField("最終同期(NTP)", bot.getExecutor().getProvider().getLastNTPTime()
+								.map(ZonedDateTime::toString)
+								.orElse("未同期"), false)
+						.addField("現在時刻(コンピューター)", ZonedDateTime.now(TimeProvider.ZONE_ID).toString(), false)
+						.addField("現在時刻(オフセット)", bot.getExecutor().getProvider().now().toString(), false)
+						.addField("オフセット(ミリ秒)", String.valueOf(bot.getExecutor().getProvider().getOffset()), false)))
+				.then();
+	}
+
+}
