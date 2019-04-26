702d61ccddc63fe354526b807b2c4ea6846e854d
sjcl <fruit@bebehp.com>
2019-04-26 14:52:00 +0900

    add monitor command

---
+++ src/main/java/net/teamfruit/eewbot/command/MonitorCommand.java
@@ -0,0 +1,25 @@
+package net.teamfruit.eewbot.command;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import net.teamfruit.eewbot.EEWBot;
+import net.teamfruit.eewbot.entity.Monitor;
+import net.teamfruit.eewbot.gateway.MonitorGateway;
+import reactor.core.publisher.Mono;
+
+public class MonitorCommand implements ICommand {
+
+	@Override
+	public Mono<Void> execute(final EEWBot bot, final MessageCreateEvent event, final String[] args) {
+		bot.getExecutor().getExecutor().execute(new MonitorGateway(bot.getExecutor().getProvider()) {
+
+			@Override
+			public void onNewData(final Monitor data) {
+				event.getMessage().getChannel()
+						.flatMap(channel -> channel.createMessage(data.createMessage()))
+						.subscribe();
+			}
+		});
+		return Mono.empty();
+	}
+
+}
