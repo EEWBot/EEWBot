2ec0ed9906e5ee8191e7321353ee9a93f2ad5428
sjcl <fruit@bebehp.com>
2019-04-26 11:24:34 +0900

    command interface

---
+++ src/main/java/net/teamfruit/eewbot/command/ICommand.java
@@ -0,0 +1,11 @@
+package net.teamfruit.eewbot.command;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import net.teamfruit.eewbot.EEWBot;
+import reactor.core.publisher.Mono;
+
+public interface ICommand {
+
+	Mono<Void> execute(EEWBot bot, MessageCreateEvent event, String[] args);
+
+}
