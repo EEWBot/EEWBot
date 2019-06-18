e7346e92fc2b610fa3ce5764bc4662ba16a382ce
sjcl <fruit@bebehp.com>
2019-06-18 23:19:01 +0900

    symbol intensity

--- src/main/java/net/teamfruit/eewbot/entity/SeismicIntensity.java
+++ src/main/java/net/teamfruit/eewbot/entity/SeismicIntensity.java
@@ -4,33 +4,37 @@ import java.awt.Color;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang3.StringUtils;
-
 public enum SeismicIntensity {
-	ONE("震度1", new Color(127, 140, 141)),
-	TWO("震度2", new Color(41, 128, 185)),
-	THREE("震度3", new Color(39, 174, 96)),
-	FOUR("震度4", new Color(221, 176, 0)),
-	FIVE_MINUS("震度5弱", new Color(230, 126, 34)),
-	FIVE_PLUS("震度5強", new Color(182, 72, 18)),
-	SIX_MINUS("震度6弱", new Color(255, 118, 188)),
-	SIX_PLUS("震度6強", new Color(255, 46, 18)),
-	SEVEN("震度7", new Color(114, 0, 172));
+	ONE("1", "1", new Color(127, 140, 141)),
+	TWO("2", "2", new Color(41, 128, 185)),
+	THREE("3", "3", new Color(39, 174, 96)),
+	FOUR("4", "4", new Color(221, 176, 0)),
+	FIVE_MINUS("5弱", "5-", new Color(230, 126, 34)),
+	FIVE_PLUS("5強", "5+", new Color(182, 72, 18)),
+	SIX_MINUS("6弱", "6-", new Color(255, 118, 188)),
+	SIX_PLUS("6強", "6+", new Color(255, 46, 18)),
+	SEVEN("7", "7", new Color(114, 0, 172));
 
 	private final String name;
+	private final String symbol;
 	private final Color color;
 
-	private SeismicIntensity(final String name, final Color color) {
+	private SeismicIntensity(final String name, final String symbol, final Color color) {
 		this.name = name;
+		this.symbol = symbol;
 		this.color = color;
+	}
+
+	public String getSimple() {
+		return this.name;
+	}
+
+	public String getSymbolIntensity() {
+		return this.symbol;
 	}
 
 	public Color getColor() {
 		return this.color;
-	}
-
-	public String getSimple() {
-		return StringUtils.substring(this.name, 2, name().length());
 	}
 
 	@Override
@@ -39,7 +43,7 @@ public enum SeismicIntensity {
 	}
 
 	public static Optional<SeismicIntensity> get(final String name) {
-		return Optional.ofNullable(Stream.of(values()).filter(value -> value.toString().equals(name)||value.getSimple().equals(name)).findAny().orElse(null));
+		return Optional.ofNullable(Stream.of(values()).filter(value -> value.getSimple().equals(name)||value.getSymbolIntensity().equals(name)).findAny().orElse(null));
 	}
 
 }
