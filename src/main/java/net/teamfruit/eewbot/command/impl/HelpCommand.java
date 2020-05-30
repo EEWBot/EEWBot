157b64941690a39b79a983bb6cefeaa8964e562c
sjcl <fruit@bebehp.com>
2020-05-30 10:42:58 +0900

    Update help

--- src/main/java/net/teamfruit/eewbot/command/impl/HelpCommand.java
+++ src/main/java/net/teamfruit/eewbot/command/impl/HelpCommand.java
@@ -20,6 +20,7 @@ public class HelpCommand implements ICommand {
 						.addField("remove", "eewbot.cmd.help.field.remove.value", true)
 						.addField("monitor", "eewbot.cmd.help.field.monitor.value", true)
 						.addField("quakeinfo", "eewbot.cmd.help.field.quakeinfo", true)
+						.addField("sensitivity", "eewbot.cmd.help.field.sensitivity", true)
 						.addField("time", "eewbot.cmd.help.field.time.value", true)
 						.addField("timefix", "eewbot.cmd.help.field.timefix.value", true)
 						.addField("joinserver", "eewbot.cmd.help.field.joinserver.value", true)
