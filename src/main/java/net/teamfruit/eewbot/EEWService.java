1dbf398d3cf2a678546fc59ed8b08f7810932af1
sjcl <fruit@bebehp.com>
2020-07-29 05:52:22 +0900

    simple sendMessage error logs

--- src/main/java/net/teamfruit/eewbot/EEWService.java
+++ src/main/java/net/teamfruit/eewbot/EEWService.java
@@ -10,6 +10,7 @@ import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.entity.channel.TextChannel;
 import discord4j.core.spec.MessageCreateSpec;
+import discord4j.rest.http.client.ClientException;
 import net.teamfruit.eewbot.registry.Channel;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -34,7 +35,17 @@ public class EEWService {
 				.map(entry -> this.gateway.getChannelById(Snowflake.of(entry.getKey()))
 						.filter(c -> c.getType()==discord4j.core.object.entity.channel.Channel.Type.GUILD_TEXT)
 						.cast(TextChannel.class)
-						.flatMap(tc -> tc.createMessage(spec.apply(entry.getValue().lang))))
+						.flatMap(tc -> tc.createMessage(spec.apply(entry.getValue().lang)).onErrorResume(t -> {
+							if (t instanceof ClientException) {
+								final ClientException ce = (ClientException) t;
+								Log.logger.info(String.format("ClientException: GuildID=%s ChannelID=%s ChannelName=%s Message=%s",
+										tc.getGuildId().asString(),
+										tc.getId().asString(),
+										tc.getName(),
+										ce.getErrorResponse().map(r -> r.getFields().getOrDefault("message", "")).orElse("")));
+							}
+							return Mono.empty();
+						})))
 				.collect(Collectors.toList())).subscribeOn(Schedulers.parallel());
 	}
 
