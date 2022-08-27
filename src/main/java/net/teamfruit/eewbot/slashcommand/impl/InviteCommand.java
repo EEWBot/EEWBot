515124a57d97fbe018841ac7b9cbbc46a3b748ad
sjcl <bebe@sjcl.me>
2022-08-28 05:39:25 +0900

    Add invite command

---
+++ src/main/java/net/teamfruit/eewbot/slashcommand/impl/InviteCommand.java
@@ -0,0 +1,30 @@
+package net.teamfruit.eewbot.slashcommand.impl;
+
+import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandOption;
+import discord4j.discordjson.json.ApplicationCommandRequest;
+import net.teamfruit.eewbot.slashcommand.ISlashCommand;
+import reactor.core.publisher.Mono;
+
+public class InviteCommand implements ISlashCommand {
+
+
+    @Override
+    public String getCommandName() {
+        return "invite";
+    }
+
+    @Override
+    public ApplicationCommandRequest buildCommand() {
+        return ApplicationCommandRequest.builder()
+                .name(getCommandName())
+                .description("招待URLを取得します。")
+                .type(ApplicationCommandOption.Type.STRING.getValue())
+                .build();
+    }
+
+    @Override
+    public Mono on(ApplicationCommandInteractionEvent event) {
+        return event.reply("https://discordapp.com/oauth2/authorize?client_id="+event.getClient().getSelfId().asString()+"&scope=bot&permissions=523344");
+    }
+}
