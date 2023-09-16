140b56f9d64e09eec06a9b44e08f9c35cbfd74e3
sjcl <bebe@sjcl.me>
2023-09-16 22:40:08 +0900

    Add dmdata config entry

--- src/main/java/net/teamfruit/eewbot/registry/Config.java
+++ src/main/java/net/teamfruit/eewbot/registry/Config.java
@@ -1,85 +1,103 @@
 package net.teamfruit.eewbot.registry;
 
 public class Config {
-	private String token = "";
-	private int kyoshinDelay = 1;
-	private int quakeInfoDelay = 15;
-	private String nptServer = "time.google.com";
-	private String defaultLanuage = "ja_jp";
-	private boolean enablePermission = true;
-	private String systemChannel = "";
-	private boolean debug = false;
+    private String token = "";
+    private int kyoshinDelay = 1;
+    private int quakeInfoDelay = 15;
+    private String dmdataAPIKey = "";
+    private String dmdataReferer = "";
+    private String nptServer = "time.google.com";
+    private String defaultLanuage = "ja_jp";
+    private boolean enablePermission = true;
+    private String systemChannel = "";
+    private boolean debug = false;
 
-	public Config() {
-	}
+    public Config() {
+    }
 
-	public String getToken() {
-		return this.token;
-	}
+    public String getToken() {
+        return this.token;
+    }
 
-	public void setToken(final String token) {
-		this.token = token;
-	}
+    public void setToken(final String token) {
+        this.token = token;
+    }
 
-	public int getKyoshinDelay() {
-		return this.kyoshinDelay>=1 ? this.kyoshinDelay : 1;
-	}
+    public int getKyoshinDelay() {
+        return this.kyoshinDelay >= 1 ? this.kyoshinDelay : 1;
+    }
 
-	public void setKyoshinDelay(final int kyoshinDelay) {
-		this.kyoshinDelay = kyoshinDelay;
-	}
+    public void setKyoshinDelay(final int kyoshinDelay) {
+        this.kyoshinDelay = kyoshinDelay;
+    }
 
-	public int getQuakeInfoDelay() {
-		return this.quakeInfoDelay>=10 ? this.quakeInfoDelay : 10;
-	}
+    public int getQuakeInfoDelay() {
+        return this.quakeInfoDelay >= 10 ? this.quakeInfoDelay : 10;
+    }
 
-	public void setQuakeInfoDelay(final int quakeInfoDelay) {
-		this.quakeInfoDelay = quakeInfoDelay;
-	}
+    public void setQuakeInfoDelay(final int quakeInfoDelay) {
+        this.quakeInfoDelay = quakeInfoDelay;
+    }
 
-	public String getNptServer() {
-		return this.nptServer;
-	}
+    public String getDmdataAPIKey() {
+        return dmdataAPIKey;
+    }
 
-	public void setNptServer(final String nptServer) {
-		this.nptServer = nptServer;
-	}
+    public void setDmdataAPIKey(String dmdataAPIKey) {
+        this.dmdataAPIKey = dmdataAPIKey;
+    }
 
-	public String getDefaultLanuage() {
-		return this.defaultLanuage;
-	}
+    public String getDmdataReferer() {
+        return dmdataReferer;
+    }
 
-	public void setDefaultLanuage(final String defaultLanuage) {
-		this.defaultLanuage = defaultLanuage;
-	}
+    public void setDmdataReferer(String dmdataReferer) {
+        this.dmdataReferer = dmdataReferer;
+    }
 
-	public boolean isEnablePermission() {
-		return this.enablePermission;
-	}
+    public String getNptServer() {
+        return this.nptServer;
+    }
 
-	public void setEnablePermission(final boolean enablePermission) {
-		this.enablePermission = enablePermission;
-	}
+    public void setNptServer(final String nptServer) {
+        this.nptServer = nptServer;
+    }
 
-	public String getSystemChannel() {
-		return this.systemChannel;
-	}
+    public String getDefaultLanuage() {
+        return this.defaultLanuage;
+    }
 
-	public void setSystemChannel(final String systemChannel) {
-		this.systemChannel = systemChannel;
-	}
+    public void setDefaultLanuage(final String defaultLanuage) {
+        this.defaultLanuage = defaultLanuage;
+    }
 
-	public boolean isDebug() {
-		return this.debug;
-	}
+    public boolean isEnablePermission() {
+        return this.enablePermission;
+    }
 
-	public void setDebug(final boolean debug) {
-		this.debug = debug;
-	}
+    public void setEnablePermission(final boolean enablePermission) {
+        this.enablePermission = enablePermission;
+    }
 
-	@Override
-	public String toString() {
-		return "Config [token="+this.token+", kyoshinDelay="+this.kyoshinDelay+", quakeInfoDelay="+this.quakeInfoDelay+", nptServer="+this.nptServer+", enablePermission="+this.enablePermission+", debug="+this.debug+"]";
-	}
+    public String getSystemChannel() {
+        return this.systemChannel;
+    }
+
+    public void setSystemChannel(final String systemChannel) {
+        this.systemChannel = systemChannel;
+    }
+
+    public boolean isDebug() {
+        return this.debug;
+    }
+
+    public void setDebug(final boolean debug) {
+        this.debug = debug;
+    }
+
+    @Override
+    public String toString() {
+        return "Config [token=" + this.token + ", kyoshinDelay=" + this.kyoshinDelay + ", quakeInfoDelay=" + this.quakeInfoDelay + ", nptServer=" + this.nptServer + ", enablePermission=" + this.enablePermission + ", debug=" + this.debug + "]";
+    }
 
 }
