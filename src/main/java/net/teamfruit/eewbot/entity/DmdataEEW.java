d0084afce3d5731eaf80277b68d210bab186039b
sjcl <bebe@sjcl.me>
2023-09-17 13:21:57 +0900

    WebSocket base listener

---
+++ src/main/java/net/teamfruit/eewbot/entity/DmdataEEW.java
@@ -0,0 +1,11 @@
+package net.teamfruit.eewbot.entity;
+
+import discord4j.core.spec.MessageCreateSpec;
+
+public class DmdataEEW implements Entity {
+    
+    @Override
+    public MessageCreateSpec createMessage(String lang) {
+        return null;
+    }
+}
