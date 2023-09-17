ded2ce19dc670eba6fa9eca838ae80044fae93dd
sjcl <bebe@sjcl.me>
2023-09-17 13:54:49 +0900

    WebSocket ping/pong

---
+++ src/main/java/net/teamfruit/eewbot/entity/dmdataapi/ws/DmdataWSMessage.java
@@ -0,0 +1,38 @@
+package net.teamfruit.eewbot.entity.dmdataapi.ws;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DmdataWSMessage {
+
+    private Type type;
+
+    public DmdataWSMessage(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "DmdataWSMessage{" +
+                "type=" + type +
+                '}';
+    }
+
+    public enum Type {
+        @SerializedName("start")
+        START,
+        @SerializedName("ping")
+        PING,
+        @SerializedName("pong")
+        PONG,
+        @SerializedName("data")
+        DATA
+    }
+}
