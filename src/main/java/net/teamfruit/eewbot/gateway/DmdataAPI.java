3ae2b0bc57dbce3e0d35ca1499a4ea5085e18e57
sjcl <bebe@sjcl.me>
2023-09-20 22:50:02 +0900

    Refactor

---
+++ src/main/java/net/teamfruit/eewbot/gateway/DmdataAPI.java
@@ -0,0 +1,68 @@
+package net.teamfruit.eewbot.gateway;
+
+import net.teamfruit.eewbot.EEWBot;
+import net.teamfruit.eewbot.entity.dmdataapi.DmdataContract;
+import net.teamfruit.eewbot.entity.dmdataapi.DmdataError;
+import net.teamfruit.eewbot.entity.dmdataapi.DmdataSocketList;
+import net.teamfruit.eewbot.entity.dmdataapi.DmdataSocketStart;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
+
+public class DmdataAPI {
+
+    public static final String API_BASE = "https://api.dmdata.jp/v2";
+
+    private final HttpRequest.Builder requestBuilder;
+
+    public DmdataAPI(String apiKey, String origin) {
+        this.requestBuilder = HttpRequest.newBuilder()
+                .header("Authorization", "Basic " + Base64.getEncoder().encodeToString((apiKey + ":").getBytes()))
+                .header("Origin", origin)
+                .header("Content-Type", "application/json")
+                .header("User-Agent", "eewbot");
+    }
+
+    public DmdataContract contract() throws IOException, InterruptedException, DmdataGatewayException {
+        HttpRequest request = this.requestBuilder.copy().GET().uri(URI.create(API_BASE + "/contract")).build();
+        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            return EEWBot.GSON.fromJson(response.body(), DmdataContract.class);
+        } else {
+            throw new DmdataGatewayException(EEWBot.GSON.fromJson(response.body(), DmdataError.class));
+        }
+    }
+
+    public DmdataSocketList openSocketList() throws IOException, InterruptedException, DmdataGatewayException {
+        HttpRequest request = this.requestBuilder.copy().GET().uri(URI.create(API_BASE + "/socket?status=open")).build();
+        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            return EEWBot.GSON.fromJson(response.body(), DmdataSocketList.class);
+        } else {
+            throw new DmdataGatewayException(EEWBot.GSON.fromJson(response.body(), DmdataError.class));
+        }
+    }
+
+    public DmdataSocketStart.Response socketStart(DmdataSocketStart.Request body) throws IOException, InterruptedException, DmdataGatewayException {
+        HttpRequest request = this.requestBuilder.copy().POST(HttpRequest.BodyPublishers.ofString(EEWBot.GSON.toJson(body))).uri(URI.create(API_BASE + "/socket")).build();
+        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            return EEWBot.GSON.fromJson(response.body(), DmdataSocketStart.Response.class);
+        } else {
+            throw new DmdataGatewayException(EEWBot.GSON.fromJson(response.body(), DmdataError.class));
+        }
+    }
+
+    public DmdataError socketClose(String socketId) throws IOException, InterruptedException {
+        HttpRequest request = this.requestBuilder.copy().DELETE().uri(URI.create(API_BASE + "/socket/" + socketId)).build();
+        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            return EEWBot.GSON.fromJson(response.body(), DmdataError.class);
+        }
+        return null;
+    }
+
+}
--- src/main/java/net/teamfruit/eewbot/gateway/DmdataGateway.java
+++ src/main/java/net/teamfruit/eewbot/gateway/DmdataGateway.java
@@ -15,8 +15,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.WebSocket;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -27,12 +25,11 @@ import java.util.zip.ZipInputStream;
 
 public abstract class DmdataGateway implements Gateway<DmdataEEW> {
 
-    public static final String API_BASE = "https://api.dmdata.jp/v2";
     public static final String WS_BASE = "wss://ws.api.dmdata.jp/v2/websocket";
     public static final String WS_BASE_TOKYO = "wss://ws-tokyo.api.dmdata.jp/v2/websocket";
     public static final String WS_BASE_OSAKA = "wss://ws-osaka.api.dmdata.jp/v2/websocket";
 
-    private final HttpRequest.Builder requestBuilder;
+    private final DmdataAPI dmdataAPI;
     private final String appName;
     private final boolean multiConnect;
     private final boolean debug;
@@ -42,12 +39,8 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
 
     private final Map<String, DmdataEEW> prev = new ConcurrentHashMap<>();
 
-    public DmdataGateway(String apiKey, String origin, long appId, boolean multiConnect, boolean debug) {
-        this.requestBuilder = HttpRequest.newBuilder()
-                .header("Authorization", "Basic " + Base64.getEncoder().encodeToString((apiKey + ":").getBytes()))
-                .header("Origin", origin)
-                .header("Content-Type", "application/json")
-                .header("User-Agent", "eewbot");
+    public DmdataGateway(DmdataAPI api, long appId, boolean multiConnect, boolean debug) {
+        this.dmdataAPI = api;
         this.appName = "eewbot" + "-" + Long.toHexString(appId);
         this.multiConnect = multiConnect;
         this.debug = debug;
@@ -66,7 +59,7 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
         try {
             Thread.currentThread().setName("eewbot-dmdata-thread");
 
-            DmdataContract contract = contract();
+            DmdataContract contract = this.dmdataAPI.contract();
             Log.logger.info(contract.toString());
 
             boolean hasForecastContract = contract.getItems().stream().anyMatch(item -> item.getClassification().equals("eew.forecast"));
@@ -78,58 +71,19 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
                 return;
             }
 
-            DmdataSocketList socketList = openSocketList();
+            DmdataSocketList socketList = this.dmdataAPI.openSocketList();
             Log.logger.info(socketList.toString());
             if (multiConnect) {
-                this.webSocket1 = connectWebSocket(WS_BASE_TOKYO, this.appName + "-1", hasForecastContract, openSocketList());
-                this.webSocket2 = connectWebSocket(WS_BASE_OSAKA, this.appName + "-2", hasForecastContract, openSocketList());
+                this.webSocket1 = connectWebSocket(WS_BASE_TOKYO, this.appName + "-1", hasForecastContract, socketList);
+                this.webSocket2 = connectWebSocket(WS_BASE_OSAKA, this.appName + "-2", hasForecastContract, socketList);
             } else {
-                this.webSocket1 = connectWebSocket(WS_BASE, this.appName + "-1", hasForecastContract, openSocketList());
+                this.webSocket1 = connectWebSocket(WS_BASE, this.appName + "-1", hasForecastContract, socketList);
             }
         } catch (EEWGatewayException e) {
             onError(e);
         } catch (IOException | InterruptedException e) {
             onError(new EEWGatewayException("Failed to connect to DMDATA", e));
         }
-    }
-
-    private DmdataContract contract() throws IOException, InterruptedException, DmdataGatewayException {
-        HttpRequest request = this.requestBuilder.copy().GET().uri(URI.create(API_BASE + "/contract")).build();
-        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() == 200) {
-            return EEWBot.GSON.fromJson(response.body(), DmdataContract.class);
-        } else {
-            throw new DmdataGatewayException(EEWBot.GSON.fromJson(response.body(), DmdataError.class));
-        }
-    }
-
-    private DmdataSocketList openSocketList() throws IOException, InterruptedException, DmdataGatewayException {
-        HttpRequest request = this.requestBuilder.copy().GET().uri(URI.create(API_BASE + "/socket?status=open")).build();
-        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() == 200) {
-            return EEWBot.GSON.fromJson(response.body(), DmdataSocketList.class);
-        } else {
-            throw new DmdataGatewayException(EEWBot.GSON.fromJson(response.body(), DmdataError.class));
-        }
-    }
-
-    private DmdataSocketStart.Response socketStart(DmdataSocketStart.Request body) throws IOException, InterruptedException, DmdataGatewayException {
-        HttpRequest request = this.requestBuilder.copy().POST(HttpRequest.BodyPublishers.ofString(EEWBot.GSON.toJson(body))).uri(URI.create(API_BASE + "/socket")).build();
-        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() == 200) {
-            return EEWBot.GSON.fromJson(response.body(), DmdataSocketStart.Response.class);
-        } else {
-            throw new DmdataGatewayException(EEWBot.GSON.fromJson(response.body(), DmdataError.class));
-        }
-    }
-
-    private DmdataError socketClose(String socketId) throws IOException, InterruptedException {
-        HttpRequest request = this.requestBuilder.copy().DELETE().uri(URI.create(API_BASE + "/socket/" + socketId)).build();
-        HttpResponse<String> response = EEWBot.instance.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() != 200) {
-            return EEWBot.GSON.fromJson(response.body(), DmdataError.class);
-        }
-        return null;
     }
 
     private WebSocketListener connectWebSocket(String wsBaseURI, String connectionName, boolean hasForecastContract, DmdataSocketList socketList) throws EEWGatewayException {
@@ -147,7 +101,7 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
 
         DmdataSocketStart.Response socketStart;
         try {
-            socketStart = socketStart(new DmdataSocketStart.Request.Builder()
+            socketStart = this.dmdataAPI.socketStart(new DmdataSocketStart.Request.Builder()
                     .setAppName(connectionName)
                     .setClassifications(Collections.singletonList(hasForecastContract ? "eew.forecast" : "eew.warning"))
                     .setTypes(types)
@@ -171,7 +125,7 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
     private void reconnectWebSocket(String wsBaseURI, String connectionName, boolean hasForecastContract) throws EEWGatewayException {
         try {
             Log.logger.info("DMDATA WebSocket reconnecting");
-            connectWebSocket(wsBaseURI, connectionName, hasForecastContract, openSocketList());
+            connectWebSocket(wsBaseURI, connectionName, hasForecastContract, this.dmdataAPI.openSocketList());
         } catch (IOException | InterruptedException e) {
             throw new EEWGatewayException("Failed to reconnect to DMDATA", e);
         }
@@ -182,7 +136,7 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
             for (DmdataSocketList.Item item : socketList.getItems()) {
                 if (StringUtils.equals(item.getAppName(), connectionName)) {
                     Log.logger.info("DMDATA Socket closing: {}", item.getId());
-                    DmdataError closeError = socketClose(String.valueOf(item.getId()));
+                    DmdataError closeError = this.dmdataAPI.socketClose(String.valueOf(item.getId()));
                     if (closeError != null) {
                         if (closeError.getError().getCode() == 404) {
                             Log.logger.info("DMDATA Socket already closed: {}", item.getId());
@@ -347,7 +301,7 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
         public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
             Log.logger.info("DMDATA WebSocket {}: closed: {} {}", connectionName, statusCode, reason);
             try {
-                closeWebSocketIfExist(openSocketList(), connectionName);
+                closeWebSocketIfExist(DmdataGateway.this.dmdataAPI.openSocketList(), connectionName);
                 reconnectWebSocket(wsBaseURI, connectionName, hasForecastContract);
             } catch (EEWGatewayException e) {
                 DmdataGateway.this.onError(e);
@@ -363,7 +317,7 @@ public abstract class DmdataGateway implements Gateway<DmdataEEW> {
             DmdataGateway.this.onError(new EEWGatewayException("DMDATA WebSocket error", error));
             if (webSocket.isOutputClosed() || webSocket.isInputClosed()) {
                 try {
-                    closeWebSocketIfExist(openSocketList(), connectionName);
+                    closeWebSocketIfExist(DmdataGateway.this.dmdataAPI.openSocketList(), connectionName);
                     reconnectWebSocket(wsBaseURI, connectionName, hasForecastContract);
                 } catch (EEWGatewayException e) {
                     DmdataGateway.this.onError(e);
