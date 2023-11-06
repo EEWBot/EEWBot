8020387a4af6c1d1c1f44918409912618aed5668
sjcl <bebe@sjcl.me>
2023-11-07 00:03:15 +0900

    Reply

--- src/main/java/net/teamfruit/eewbot/slashcommand/impl/TestMessageSlashCommand.java
+++ src/main/java/net/teamfruit/eewbot/slashcommand/impl/TestMessageSlashCommand.java
@@ -1,9 +1,12 @@
 package net.teamfruit.eewbot.slashcommand.impl;
 
 import discord4j.core.event.domain.interaction.ApplicationCommandInteractionEvent;
+import discord4j.core.spec.MessageCreateSpec;
 import discord4j.discordjson.json.ApplicationCommandRequest;
 import net.teamfruit.eewbot.EEWBot;
+import net.teamfruit.eewbot.i18n.I18n;
 import net.teamfruit.eewbot.slashcommand.ISlashCommand;
+import net.teamfruit.eewbot.slashcommand.SlashCommandUtils;
 import reactor.core.publisher.Mono;
 
 public class TestMessageSlashCommand implements ISlashCommand {
@@ -23,6 +26,18 @@ public class TestMessageSlashCommand implements ISlashCommand {
 
     @Override
     public Mono<Void> on(EEWBot bot, ApplicationCommandInteractionEvent event, String lang) {
-        return event.reply();
+        return event.deferReply()
+                .then(bot.getService().sendMessange(event.getInteraction().getChannelId().asLong(), this::createMessage)
+                        .flatMap(message -> event.createFollowup(I18n.INSTANCE.get(lang, "eewbot.scmd.testmessage.success")))
+                        .then());
+    }
+
+    private MessageCreateSpec createMessage(String lang) {
+        return MessageCreateSpec.builder()
+                .addEmbed(SlashCommandUtils.createEmbed(lang)
+                        .title("eewbot.scmd.testmessage.title")
+                        .description("eewbot.scmd.testmessage.desc")
+                        .build())
+                .build();
     }
 }
