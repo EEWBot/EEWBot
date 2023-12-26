1a8d304c52c6cd81f2997ba8cd05d19e89fbeb8d
sjcl <bebe@sjcl.me>
2023-12-27 06:36:10 +0900

    refactor

--- src/main/java/net/teamfruit/eewbot/slashcommand/SlashCommandUtils.java
+++ src/main/java/net/teamfruit/eewbot/slashcommand/SlashCommandUtils.java
@@ -3,9 +3,11 @@ package net.teamfruit.eewbot.slashcommand;
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.event.domain.message.ReactionAddEvent;
+import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.rest.util.Color;
 import net.teamfruit.eewbot.EEWBot;
 import net.teamfruit.eewbot.i18n.I18nEmbedCreateSpec;
+import net.teamfruit.eewbot.i18n.IEmbedBuilder;
 
 import java.util.Optional;
 
@@ -26,14 +28,14 @@ public class SlashCommandUtils {
         return getLanguage(bot, event.getGuildId());
     }
 
-    public static I18nEmbedCreateSpec.Builder createEmbed(final String lang) {
+    public static IEmbedBuilder<EmbedCreateSpec> createEmbed(final String lang) {
         return I18nEmbedCreateSpec.builder(lang)
                 .color(Color.of(7506394))
                 .author(EEWBot.instance.getUsername(), "https://github.com/Team-Fruit/EEWBot", EEWBot.instance.getAvatarUrl())
                 .footer("Team-Fruit/EEWBot", "http://i.imgur.com/gFHBoZA.png");
     }
 
-    public static I18nEmbedCreateSpec.Builder createErrorEmbed(final String lang) {
+    public static IEmbedBuilder<EmbedCreateSpec> createErrorEmbed(final String lang) {
         return I18nEmbedCreateSpec.builder(lang)
                 .color(Color.of(255, 64, 64))
                 .author(EEWBot.instance.getUsername(), "https://github.com/Team-Fruit/EEWBot", EEWBot.instance.getAvatarUrl())
