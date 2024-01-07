91bcf18e5a0461789be5ecfd75ddd970cd6670c1
sjcl <bebe@sjcl.me>
2024-01-08 00:14:25 +0900

    Add ChannelBase#equals

--- src/main/java/net/teamfruit/eewbot/registry/ChannelBase.java
+++ src/main/java/net/teamfruit/eewbot/registry/ChannelBase.java
@@ -2,6 +2,8 @@ package net.teamfruit.eewbot.registry;
 
 import reactor.util.annotation.Nullable;
 
+import java.util.Objects;
+
 public class ChannelBase {
 
     protected Webhook webhook;
@@ -26,6 +28,24 @@ public class ChannelBase {
 
     void setLang(String lang) {
         this.lang = lang;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChannelBase that = (ChannelBase) o;
+
+        if (!Objects.equals(this.webhook, that.webhook)) return false;
+        return Objects.equals(this.lang, that.lang);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.webhook != null ? this.webhook.hashCode() : 0;
+        result = 31 * result + (this.lang != null ? this.lang.hashCode() : 0);
+        return result;
     }
 
     @Override
