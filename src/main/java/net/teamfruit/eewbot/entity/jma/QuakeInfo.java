7e9a4bbe077a2f18fc3f520f4618c4e90c72167a
sjcl <bebe@sjcl.me>
2024-04-26 02:07:06 +0900

    Change /quakeinfo to JMA source

--- src/main/java/net/teamfruit/eewbot/entity/jma/QuakeInfo.java
+++ src/main/java/net/teamfruit/eewbot/entity/jma/QuakeInfo.java
@@ -1,10 +1,11 @@
 package net.teamfruit.eewbot.entity.jma;
 
+import net.teamfruit.eewbot.entity.Entity;
 import net.teamfruit.eewbot.entity.SeismicIntensity;
 
 import java.util.Optional;
 
-public interface QuakeInfo extends IJMAReport {
+public interface QuakeInfo extends Entity, IJMAReport {
 
     Optional<SeismicIntensity> getMaxInt();
 }
