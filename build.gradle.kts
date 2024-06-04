93007191ad5d19a3a3c20f885a0d44923d58fab5
sjcl <bebe@sjcl.me>
2024-06-05 04:20:08 +0900

    shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
+    java
     `java-library`
+    alias(libs.plugins.shadow.jar)
 }
 
 repositories {
@@ -33,3 +37,18 @@ group = "net.teamfruit"
 version = "2.7.7"
 description = "EEWBot"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        manifest {
+            archiveClassifier.set("")
+            mergeServiceFiles()
+            attributes(mapOf("Main-Class" to "net.teamfruit.eewbot.EEWBot"))
+            minimize {
+                exclude(dependency("ch.qos.logback:logback-classic:.*"))
+                exclude(dependency("com.fasterxml.woodstox:woodstox-core:.*"))
+            }
+        }
+    }
+}
+
