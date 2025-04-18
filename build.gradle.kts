63b080ff94a63ef9e1a54b4dbc7171a63881ead6
sjcl <bebe@sjcl.me>
2025-04-19 01:20:15 +0900

    Add Wire plugin and update dependency management in Gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     java
     `java-library`
+
+    alias(libs.plugins.com.squareup.wire)
     alias(libs.plugins.shadow.jar)
-}
-
-repositories {
-    mavenLocal()
-    maven {
-        url = uri("https://jcenter.bintray.com")
-    }
-
-    maven {
-        url = uri("https://jitpack.io")
-    }
-
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
 }
 
 dependencies {
@@ -32,12 +19,27 @@ dependencies {
     api(libs.com.fasterxml.jackson.dataformat.jackson.dataformat.xml)
     api(libs.com.fasterxml.jackson.datatype.jackson.datatype.jsr310)
     api(libs.redis.clients.jedis)
+
+    implementation(libs.wire.runtime)
 }
 
 group = "net.teamfruit"
 version = "2.7.7"
 description = "EEWBot"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+wire {
+    java {
+    }
+}
+
+sourceSets {
+    named("main") {
+        java {
+            srcDir("build/generated/source/wire/main/java")
+        }
+    }
+}
 
 tasks {
     named("jar") {
