ea8bca5ce52fe5fb8a575c67e5270ccb453d0285
sjcl <bebe@sjcl.me>
2025-04-20 22:47:18 +0900

    Refactor RendererQueryGenerator to use updated QuakeDataFactory method and improve logging

--- src/main/java/net/teamfruit/eewbot/RendererQueryGenerator.java
+++ src/main/java/net/teamfruit/eewbot/RendererQueryGenerator.java
@@ -5,6 +5,7 @@ import net.teamfruit.eewbot.entity.jma.telegram.VXSE53Impl;
 import net.teamfruit.eewbot.entity.renderer.QuakeDataFactory;
 
 import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 
 public class RendererQueryGenerator {
     public static void main(String[] args) throws Exception {
@@ -17,13 +18,8 @@ public class RendererQueryGenerator {
 
         try (var inputStream = new FileInputStream(path)) {
             VXSE53 report = EEWBot.XML_MAPPER.readValue(inputStream, VXSE53Impl.class);
-            String data = QuakeDataFactory.generateQuakePrefectureData(
-                    hmacKey,
-                    report.getOriginTime(),
-                    report.getCoordinate(),
-                    report.getObservation()
-            );
-            System.out.println(data);
+            String data = QuakeDataFactory.generate(hmacKey.getBytes(StandardCharsets.UTF_8), report);
+            Log.logger.info(data);
         }
     }
 }
