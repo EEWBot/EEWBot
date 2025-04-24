6f32f9efacf0acc687d242a3b6b8cb8a94c6290e
sjcl <bebe@sjcl.me>
2025-04-25 01:20:11 +0900

    Refactor language initialization to improve error handling and streamline loading process

--- src/main/java/net/teamfruit/eewbot/i18n/I18n.java
+++ src/main/java/net/teamfruit/eewbot/i18n/I18n.java
@@ -3,10 +3,12 @@ package net.teamfruit.eewbot.i18n;
 import com.google.gson.reflect.TypeToken;
 import net.teamfruit.eewbot.EEWBot;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class I18n {
 
@@ -23,15 +25,24 @@ public class I18n {
     public void init(String defaultLanguage) {
         this.defaultLanguage = defaultLanguage;
 
-        this.languages = EEWBot.GSON.fromJson(new InputStreamReader(Objects.requireNonNull(I18n.class.getResourceAsStream("/lang/languages.json"))), new TypeToken<Map<String, String>>() {
-        }.getType());
-
-        this.languages.keySet().forEach(key -> {
-            final Map<String, String> map = EEWBot.GSON.fromJson(new InputStreamReader(Objects.requireNonNull(I18n.class.getResourceAsStream("/lang/" + key + ".json"))), new TypeToken<Map<String, String>>() {
+        try (InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(I18n.class.getResourceAsStream("/lang/languages.json")))) {
+            Map<String, String> languages = EEWBot.GSON.fromJson(reader, new TypeToken<Map<String, String>>() {
             }.getType());
 
-            this.langMap.put(key.toLowerCase(), map);
-        });
+            languages.keySet().forEach(key -> {
+                try (InputStreamReader langReader = new InputStreamReader(Objects.requireNonNull(I18n.class.getResourceAsStream("/lang/" + key + ".json")))) {
+                    Map<String, String> langMap = EEWBot.GSON.fromJson(langReader, new TypeToken<Map<String, String>>() {
+                    }.getType());
+                    this.langMap.put(key.toLowerCase(), langMap);
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to load: " + key + ".json", e);
+                }
+            });
+
+            this.languages = languages.entrySet().stream().collect(Collectors.toMap(e -> e.getKey().toLowerCase(), Map.Entry::getValue));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load language.json: ", e);
+        }
     }
 
     public Map<String, String> getLanguages() {
