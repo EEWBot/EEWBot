3a69af6d80f4246b0ab59747c2e5f8bd1e45f493
sjcl <bebe@sjcl.me>
2025-04-27 05:06:11 +0900

    Add WebhookSenderRequest class for handling webhook requests

---
+++ src/main/java/net/teamfruit/eewbot/entity/webhooksender/WebhookSenderRequest.java
@@ -0,0 +1,22 @@
+package net.teamfruit.eewbot.entity.webhooksender;
+
+import net.teamfruit.eewbot.entity.discord.DiscordWebhook;
+
+import java.util.List;
+
+public class WebhookSenderRequest {
+
+    public final List<String> targets;
+    public final DiscordWebhook body;
+    public final int retry_limit;
+
+    public WebhookSenderRequest(List<String> targets, DiscordWebhook body, int retry_limit) {
+        this.targets = targets;
+        this.body = body;
+        this.retry_limit = retry_limit;
+    }
+
+    public WebhookSenderRequest(List<String> targets, DiscordWebhook body) {
+        this(targets, body, 10);
+    }
+}
