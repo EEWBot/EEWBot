ffdc9c57d418da0a14446e8bc495fdf06d88cce1
sjcl <bebe@sjcl.me>
2026-03-11 02:51:09 +0900

    Translate TsunamiCategory enum values to Japanese and update comments for clarity

--- src/main/java/net/teamfruit/eewbot/entity/TsunamiCategory.java
+++ src/main/java/net/teamfruit/eewbot/entity/TsunamiCategory.java
@@ -2,17 +2,30 @@ package net.teamfruit.eewbot.entity;
 
 import discord4j.rest.util.Color;
 
+@SuppressWarnings("NonAsciiCharacters")
 public enum TsunamiCategory {
-    NONE("00", 0, Color.of(127, 140, 141)),
-    WARNING_CANCELLED("50", 0, Color.of(127, 140, 141)),
-    WARNING("51", 3, Color.of(255, 40, 0)),
-    MAJOR_WARNING("52", 4, Color.of(200, 0, 255)),
-    MAJOR_WARNING_FIRING("53", 4, Color.of(200, 0, 255)),
-    ADVISORY_CANCELLED("60", 0, Color.of(127, 140, 141)),
-    ADVISORY("62", 2, Color.of(255, 40, 0)),
-    FORECAST("71", 1, Color.of(0, 191, 255)),
-    FORECAST_FROM_ADVISORY("72", 1, Color.of(0, 191, 255)),
-    FORECAST_FROM_WARNING("73", 1, Color.of(0, 191, 255));
+    津波なし("00", 0, Color.of(127, 140, 141)),
+    /**
+     * 大津波警報または津波警報の解除
+     */
+    警報解除("50", 0, Color.of(127, 140, 141)),
+    津波警報("51", 3, Color.of(255, 40, 0)),
+    大津波警報("52", 4, Color.of(200, 0, 255)),
+    /**
+     * 大津波警報の新規発表または切替
+     */
+    大津波警報_発表("53", 4, Color.of(200, 0, 255)),
+    津波注意報解除("60", 0, Color.of(127, 140, 141)),
+    津波注意報("62", 2, Color.of(255, 40, 0)),
+    津波予報("71", 1, Color.of(0, 191, 255)),
+    /**
+     * 津波注意報解除、津波予報（若干の海面変動）への切替
+     */
+    津波予報_津波注意報解除("72", 1, Color.of(0, 191, 255)),
+    /**
+     * 大津波警報または津波警報の解除、津波予報（若干の海面変動）への切替
+     */
+    津波予報_大津波警報または津波警報解除("73", 1, Color.of(0, 191, 255));
 
     private final String code;
     private final int priority;
@@ -28,7 +41,7 @@ public enum TsunamiCategory {
         for (TsunamiCategory v : values()) {
             if (v.code.equals(code)) return v;
         }
-        return NONE;
+        throw new IllegalArgumentException("Invalid code: " + code);
     }
 
     public String getCode() {
