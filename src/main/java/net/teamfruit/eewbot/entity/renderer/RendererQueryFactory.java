e068c2e8f65be6a90175ef36f4416628285d505c
sjcl <bebe@sjcl.me>
2026-03-23 21:44:38 +0900

    Update eew renderer proto

--- src/main/java/net/teamfruit/eewbot/entity/renderer/RendererQueryFactory.java
+++ src/main/java/net/teamfruit/eewbot/entity/renderer/RendererQueryFactory.java
@@ -1,5 +1,9 @@
 package net.teamfruit.eewbot.entity.renderer;
 
+import net.eewbot.CodeArray;
+import net.eewbot.Epicenter;
+import net.eewbot.QuakePrefectureV0;
+import net.eewbot.TsunamiForecastV0;
 import net.eewbot.base32768j.Base32768;
 import net.teamfruit.eewbot.entity.SeismicIntensity;
 import net.teamfruit.eewbot.entity.TsunamiCategory;
@@ -7,10 +11,6 @@ import net.teamfruit.eewbot.entity.jma.telegram.common.Coordinate;
 import net.teamfruit.eewbot.entity.jma.telegram.seis.*;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
-import quake_prefecture_v0.CodeArray;
-import quake_prefecture_v0.Epicenter;
-import quake_prefecture_v0.QuakePrefectureData;
-import tsunami_v0.TsunamiForecastData;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -30,19 +30,19 @@ public class RendererQueryFactory {
     private static final byte VERSIONED_TYPE_TSUNAMI = 1;
     private static final String HMAC_ALGO = "HmacSHA1";
 
-    private static final EnumMap<SeismicIntensity, BiConsumer<QuakePrefectureData.Builder, CodeArray>> SETTER_MAP =
+    private static final EnumMap<SeismicIntensity, BiConsumer<QuakePrefectureV0.Builder, CodeArray>> SETTER_MAP =
             new EnumMap<>(SeismicIntensity.class);
 
     static {
-        SETTER_MAP.put(SeismicIntensity.ONE, QuakePrefectureData.Builder::one);
-        SETTER_MAP.put(SeismicIntensity.TWO, QuakePrefectureData.Builder::two);
-        SETTER_MAP.put(SeismicIntensity.THREE, QuakePrefectureData.Builder::three);
-        SETTER_MAP.put(SeismicIntensity.FOUR, QuakePrefectureData.Builder::four);
-        SETTER_MAP.put(SeismicIntensity.FIVE_MINUS, QuakePrefectureData.Builder::five_minus);
-        SETTER_MAP.put(SeismicIntensity.FIVE_PLUS, QuakePrefectureData.Builder::five_plus);
-        SETTER_MAP.put(SeismicIntensity.SIX_MINUS, QuakePrefectureData.Builder::six_minus);
-        SETTER_MAP.put(SeismicIntensity.SIX_PLUS, QuakePrefectureData.Builder::six_plus);
-        SETTER_MAP.put(SeismicIntensity.SEVEN, QuakePrefectureData.Builder::seven);
+        SETTER_MAP.put(SeismicIntensity.ONE, QuakePrefectureV0.Builder::one);
+        SETTER_MAP.put(SeismicIntensity.TWO, QuakePrefectureV0.Builder::two);
+        SETTER_MAP.put(SeismicIntensity.THREE, QuakePrefectureV0.Builder::three);
+        SETTER_MAP.put(SeismicIntensity.FOUR, QuakePrefectureV0.Builder::four);
+        SETTER_MAP.put(SeismicIntensity.FIVE_MINUS, QuakePrefectureV0.Builder::five_minus);
+        SETTER_MAP.put(SeismicIntensity.FIVE_PLUS, QuakePrefectureV0.Builder::five_plus);
+        SETTER_MAP.put(SeismicIntensity.SIX_MINUS, QuakePrefectureV0.Builder::six_minus);
+        SETTER_MAP.put(SeismicIntensity.SIX_PLUS, QuakePrefectureV0.Builder::six_plus);
+        SETTER_MAP.put(SeismicIntensity.SEVEN, QuakePrefectureV0.Builder::seven);
     }
 
     private final String baseURL;
@@ -111,10 +111,10 @@ public class RendererQueryFactory {
         return codeMap;
     }
 
-    private void applyCodeMap(QuakePrefectureData.Builder builder, Map<SeismicIntensity, List<Integer>> codeMap) {
+    private void applyCodeMap(QuakePrefectureV0.Builder builder, Map<SeismicIntensity, List<Integer>> codeMap) {
         codeMap.forEach((intensity, codes) -> {
             CodeArray codeArray = new CodeArray.Builder().codes(codes).build();
-            BiConsumer<QuakePrefectureData.Builder, CodeArray> setter = SETTER_MAP.get(intensity);
+            BiConsumer<QuakePrefectureV0.Builder, CodeArray> setter = SETTER_MAP.get(intensity);
             if (setter != null) {
                 setter.accept(builder, codeArray);
             }
@@ -129,12 +129,12 @@ public class RendererQueryFactory {
         return buffer.array();
     }
 
-    private String generateQuakePrefectureData(Instant time, @Nullable Coordinate coordinate, @Nullable Intensity.IntensityDetail observation) {
+    private String generateQuakePrefectureV0(Instant time, @Nullable Coordinate coordinate, @Nullable Intensity.IntensityDetail observation) {
         if (coordinate == null && observation == null) {
             throw new IllegalArgumentException("Either coordinate or observation is required");
         }
 
-        QuakePrefectureData.Builder builder = new QuakePrefectureData.Builder();
+        QuakePrefectureV0.Builder builder = new QuakePrefectureV0.Builder();
         builder.time(time.getEpochSecond());
 
         if (coordinate != null) {
@@ -144,7 +144,7 @@ public class RendererQueryFactory {
         Map<SeismicIntensity, List<Integer>> codeMap = buildCodeMap(observation);
         applyCodeMap(builder, codeMap);
 
-        byte[] body = QuakePrefectureData.ADAPTER.encode(builder.build());
+        byte[] body = QuakePrefectureV0.ADAPTER.encode(builder.build());
         byte[] query = computeQuery(VERSIONED_TYPE_QUAKE_PREFECTURE, body);
         return Base32768.getEncoder().encodeToString(query);
     }
@@ -154,24 +154,12 @@ public class RendererQueryFactory {
             throw new IllegalStateException("Renderer is not available");
         }
 
-        String base32768Str = generateQuakePrefectureData(renderQuakePrefecture.getTime(), renderQuakePrefecture.getCoordinate(), renderQuakePrefecture.getIntensityDetail());
+        String base32768Str = generateQuakePrefectureV0(renderQuakePrefecture.getTime(), renderQuakePrefecture.getCoordinate(), renderQuakePrefecture.getIntensityDetail());
         String normalizedBaseURL = this.baseURL.endsWith("/") ? this.baseURL : this.baseURL + "/";
         return normalizedBaseURL + base32768Str;
     }
 
-    private tsunami_v0.Epicenter buildTsunamiEpicenter(Coordinate coordinate) {
-        Float lat = coordinate.getLat(), lon = coordinate.getLon();
-        if (lat == null || lon == null) {
-            return null;
-        }
-
-        return new tsunami_v0.Epicenter.Builder()
-                .lat_x10((int) (lat * 10))
-                .lon_x10((int) (lon * 10))
-                .build();
-    }
-
-    private String generateTsunamiData(Instant time, @Nullable Coordinate coordinate, List<TsunamiItem> forecastItems) {
+    private String generateTsunamiData(Instant time, List<Coordinate> coordinates, List<TsunamiItem> forecastItems) {
         List<Integer> forecastCodes = new ArrayList<>();
         List<Integer> advisoryCodes = new ArrayList<>();
         List<Integer> warningCodes = new ArrayList<>();
@@ -194,19 +182,24 @@ public class RendererQueryFactory {
             }
         }
 
-        TsunamiForecastData.Builder builder = new TsunamiForecastData.Builder();
+        TsunamiForecastV0.Builder builder = new TsunamiForecastV0.Builder();
         builder.time(time.getEpochSecond());
 
-        if (coordinate != null) {
-            builder.epicenter(buildTsunamiEpicenter(coordinate));
+        List<Epicenter> epicenters = new ArrayList<>();
+        for (Coordinate coordinate : coordinates) {
+            Epicenter epicenter = buildEpicenter(coordinate);
+            if (epicenter != null) {
+                epicenters.add(epicenter);
+            }
         }
+        builder.epicenter(epicenters);
 
-        builder.forecast(new tsunami_v0.CodeArray.Builder().codes(forecastCodes).build());
-        builder.advisory(new tsunami_v0.CodeArray.Builder().codes(advisoryCodes).build());
-        builder.warning(new tsunami_v0.CodeArray.Builder().codes(warningCodes).build());
-        builder.major_warning(new tsunami_v0.CodeArray.Builder().codes(majorWarningCodes).build());
+        builder.forecast(new CodeArray.Builder().codes(forecastCodes).build());
+        builder.advisory(new CodeArray.Builder().codes(advisoryCodes).build());
+        builder.warning(new CodeArray.Builder().codes(warningCodes).build());
+        builder.major_warning(new CodeArray.Builder().codes(majorWarningCodes).build());
 
-        byte[] body = TsunamiForecastData.ADAPTER.encode(builder.build());
+        byte[] body = TsunamiForecastV0.ADAPTER.encode(builder.build());
         byte[] query = computeQuery(VERSIONED_TYPE_TSUNAMI, body);
         return Base32768.getEncoder().encodeToString(query);
     }
@@ -216,7 +209,7 @@ public class RendererQueryFactory {
             throw new IllegalStateException("Renderer is not available");
         }
 
-        String base32768Str = generateTsunamiData(renderTsunami.getTime(), renderTsunami.getCoordinate(), renderTsunami.getForecastItems());
+        String base32768Str = generateTsunamiData(renderTsunami.getTime(), renderTsunami.getCoordinates(), renderTsunami.getForecastItems());
         String normalizedBaseURL = this.baseURL.endsWith("/") ? this.baseURL : this.baseURL + "/";
         return normalizedBaseURL + base32768Str;
     }
